ab6064389dda64f2a103bb78c70bc3dc4dd87a2c
Sergio <sergiomolinaflores1@gmail.com>
2023-07-14 21:22:14 -0400

    Ejercicio 13A ok

--- app/src/main/java/cl/samf/individual13_mod5/Individual13_A.kt
+++ app/src/main/java/cl/samf/individual13_mod5/Individual13_A.kt
@@ -1,5 +1,7 @@
 package cl.samf.individual13_mod5
 
+import java.text.NumberFormat
+import java.util.Currency
 import kotlin.math.ceil
 
 fun main (){
@@ -7,13 +9,21 @@ fun main (){
     println(resultado)
     var redondeoResult = redondeo(7.8)
     println(redondeoResult)
+    var resultadoMR = muestraResultado(2.2)
+    println(resultadoMR)
 }
 
 //fun suma(param1: Double, param2: Float):Double{
     //return param1+param2
 fun suma(param1: Double, param2: Float):Double=param1+param2
 
-
-
-
 fun redondeo(param1: Double)= ceil(param1)  //ceil redondea al cielo
+
+fun muestraResultado(numero: Double) : String {
+
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.setMaximumFractionDigits(2)
+    format.setCurrency(Currency.getInstance("EUR"))
+    return format.format(numero)
+
+}
